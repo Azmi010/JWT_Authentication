83bbc57ba890c1c03bffd76b921e24fbae267140
lyeiva86 <ulul10039@gmail.com>
2024-03-30 19:44:09 +0700

    auth using jwt

---
+++ Models/Person.cs
@@ -0,0 +1,12 @@
+namespace jwt.Models
+{
+    public class Person
+    {
+        public int id_person { get; set; }
+        public string? nama { get; set; }
+        public string? alamat { get; set; }
+        public string? email { get; set; }
+
+    }
+}
+
